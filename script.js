fe5668f07181289b7dcca4b02de5fc5e21fa2cd9
guiller santos <guillersantos111@gmail.com>
2025-08-11 23:23:44 +0800

    Improved UI Design changed panel profile from vertical to horizontal

--- script.js
+++ script.js
@@ -24,20 +24,3 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     }
   });
 });
-
-const observerOptions = {
-  threshold: 0.1,
-  rootMargin: '0px 0px -50px 0px'
-};
-
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add('visible');
-    }
-  });
-}, observerOptions);
-
-document.querySelectorAll('.reveal').forEach(el => {
-  observer.observe(el);
-});
